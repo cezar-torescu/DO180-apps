830c3b17209e39d828ed82732e98fbfc639d0047
Student User <student@workstation.lab.example.com>
2022-03-23 03:46:05 -0400

    changed index

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,7 +3,7 @@ print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "A change is a coming!\n";
 
 $data=array('RHEL', 'CENTOS', 'DEBIAN');
-for $data as $d {
-    print($d." is supported";
+foreach ($data as $d) {
+    print($d." is supported\n";
 }
 ?>
